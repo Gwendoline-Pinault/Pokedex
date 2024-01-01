e5470c1e8d71c51d00f840cddb7ace2b1d0e2c44
GwendolinePinault <pinaultgwendoline@gmail.com>
2024-01-01 22:00:31 +0100

    fix pokemon's label done

--- app/dataMapper.js
+++ app/dataMapper.js
@@ -14,10 +14,10 @@ const dataMapper = {
     return pokemon;
   },
   findPokemonTypes: async (id) => {
-    const queryPokemonTypes = `SELECT * FROM "pokemon_type" INNER JOIN "type" ON pokemon_type.type_id= type.id AND pokemon_numero = ${id};`;
+    const queryPokemonTypes = `SELECT * FROM "pokemon_type" INNER JOIN "type" ON pokemon_type.type_id = type.id AND pokemon_numero = ${id};`;
     const findPokemonTypes = await client.query(queryPokemonTypes);
-    const pokemonTypes = findPokemonTypes.rows[0,1];
-    return pokemonTypes;
+    const types = findPokemonTypes.rows;
+    return types;
   },
   findAllTypes: async () => {
     const queryTypes = 'SELECT * FROM "type";';
