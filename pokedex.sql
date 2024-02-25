55cb2dca5fa5ad324325deb71958594bbd4f1c08
pinault gwendoline <pinaultgwendoline@gmail.com>
2024-02-25 20:40:59 +0100

    update pokedex script

--- pokedex.sql
+++ pokedex.sql
@@ -418,7 +418,7 @@ DROP TABLE IF EXISTS "type";
 CREATE TABLE "type" (
   "id" int NOT NULL,
   "name" varchar(255) NOT NULL,
-  "color" varchar(6) NOT NULL
+  "color" varchar(7) NOT NULL
 );
 
 --
@@ -426,20 +426,20 @@ CREATE TABLE "type" (
 --
 
 INSERT INTO "type" ("id", "name", "color") VALUES
-(1, 'Acier', 'aaaabb'),
-(2, 'Combat', 'bb5544'),
-(3, 'Dragon', '7766ee'),
-(4, 'Eau', '3399ff'),
-(5, 'Électrik', 'ffbb33'),
-(6, 'Feu', 'ff4422'),
-(7, 'Glace', '77ddff'),
-(8, 'Insecte', 'aabb22'),
-(9, 'Normal', 'bbaabb'),
-(10, 'Plante', '77cc55'),
-(11, 'Poison', 'aa5599'),
-(12, 'Psy', 'ff5599'),
-(13, 'Roche', 'bbaa66'),
-(14, 'Sol', 'ddbb55'),
-(15, 'Spectre', '6666bb'),
-(16, 'Ténèbres', '665544'),
-(17, 'Vol', '6699ff');
+(1, 'Acier', '#aaaabb'),
+(2, 'Combat', '#bb5544'),
+(3, 'Dragon', '#7766ee'),
+(4, 'Eau', '#3399ff'),
+(5, 'Électrik', '#ffbb33'),
+(6, 'Feu', '#ff4422'),
+(7, 'Glace', '#77ddff'),
+(8, 'Insecte', '#aabb22'),
+(9, 'Normal', '#bbaabb'),
+(10, 'Plante', '#77cc55'),
+(11, 'Poison', '#aa5599'),
+(12, 'Psy', '#ff5599'),
+(13, 'Roche', '#bbaa66'),
+(14, 'Sol', '#ddbb55'),
+(15, 'Spectre', '#6666bb'),
+(16, 'Ténèbres', '#665544'),
+(17, 'Vol', '#6699ff');
